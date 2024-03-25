ccc1a46a87827425258b2c30d2becf7f89f31cfa
hmssameer55 <hmssameer55@gmail.com>
2024-03-25 21:46:36 +0530

    initiate webhook

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,9 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/references/nextjs/auth-middleware
 // for more information about configuring your Middleware
 
-export default authMiddleware({});
+export default authMiddleware({
+  publicRoutes: ["/api/webhooks/clerk"],
+});
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
